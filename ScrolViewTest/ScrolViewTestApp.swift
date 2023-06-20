a41af6c33572261edd207704cea36c2e1c073a6f
Jacek Kosiński <kashiash@gmail.com>
2023-06-20 22:41:18 +0200

    Initial Commit

---
+++ ScrolViewTest/ScrolViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrolViewTestApp.swift
+//  ScrolViewTest
+//
+//  Created by Jacek Kosinski U on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrolViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
